ef35fc29e9d48715585d21996ee872d62d8101e8
Charlie Mordant <cmordant1@gmail.com>
2023-05-10 18:27:51 +0200

    initial release

--- molecule/kvm/tests/test_default.py
+++ molecule/kvm/tests/test_default.py
@@ -4,7 +4,7 @@
 def test_hosts_file_contains_the_new_entry(host):
     command = r"""cat /etc/hosts | \
     egrep -c \
-    '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+poc\.osgiliath\.test'"""
+    '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+dns\.osgiliath\.test'"""
     cmd = host.run(command)
     assert int(cmd.stdout) >= 1
 
@@ -30,6 +30,12 @@ def test_hostname_is_updated(host):
     assert '1' in cmd.stdout
 
 
+def test_resolv_conf_updated(host):
+    command = r"""cat /etc/resolv.conf | grep -c nameserver"""
+    cmd = host.run(command)
+    assert int(cmd.stdout) >= 1
+
+
 def test_lookup_resolves_google(host):
     command = r"""nslookup google.fr | \
         grep -Pzoc 'Non-authoritative answer:\nName:\s+google.fr'"""
@@ -38,14 +44,14 @@ def test_lookup_resolves_google(host):
 
 
 def test_lookup_resolves_node_as_client(host):
-    command = r"""resolvectl query node0.osgiliath.test | \
-    grep -c 'link: docker0'"""
+    command = r"""nslookup node0.osgiliath.test | \
+    grep -c '192.168.1.1'"""
     cmd = host.run(command)
     assert int(cmd.stdout) >= 1
 
 
-# TODO make it work def test_lookup_resolves_reverse(host):
-#    command = r"""nslookup 192.169.0.2 | \
+# TODO Fix test def test_lookup_resolves_reverse(host):
+#    command = r"""nslookup 192.169.1.1 | \
 #    grep -c 'node0.osgiliath.test.169.192.in-addr.arpa'"""
 #    cmd = host.run(command)
 #    assert int(cmd.stdout) >= 1
---
+++ molecule/parallels/tests/test_default.py
@@ -0,0 +1,23 @@
+"""Role testing files using testinfra."""
+
+
+
+def test_lookup_resolves_google(host):
+    command = r"""nslookup google.fr | \
+        grep -Pzoc 'Non-authoritative answer:\nName:\s+google.fr'"""
+    cmd = host.run(command)
+    assert int(cmd.stdout) >= 1
+
+
+def test_lookup_resolves_node_as_client(host):
+    command = r"""resolvectl query node0.osgiliath.test | \
+    grep -c 'link: docker0'"""
+    cmd = host.run(command)
+    assert int(cmd.stdout) >= 1
+
+
+# TODO make it work def test_lookup_resolves_reverse(host):
+#    command = r"""nslookup 192.169.0.2 | \
+#    grep -c 'node0.osgiliath.test.169.192.in-addr.arpa'"""
+#    cmd = host.run(command)
+#    assert int(cmd.stdout) >= 1
